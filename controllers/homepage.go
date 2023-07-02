05b151e0bebbbb408c89dd9ad3010cb6fa49cc86
Michelle Ferris <jmichellelim@yahoo.com>
2023-07-01 21:38:58 -0400

    access google job api

--- controllers/homepage.go
+++ controllers/homepage.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	g "github.com/serpapi/google-search-results-golang"
+    "fmt"
 )
 
 // ROUTE: /
@@ -9,4 +11,23 @@ func GetHomePage(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "Hello, Gemstack community!",
 	})
+
+	func GoogleSearchAPI(c *gin.Context){
+
+      parameter := map[string]string{
+        "engine": "google_jobs",
+        "google_domain": "google.com",
+        "q": "Barista",
+        "api_key": "secret_api_key",
+      }
+
+      search := g.NewGoogleSearch(parameter, "secret_api_key")
+      results, err := search.GetJSON()
+          if err != nil {
+               fmt.Println(err.Error())
+          }
+
+          c.JSON(200, gin.H{
+          		"message": results,
+          	})
 }
